83acec0386bcf9408abb38e94bccd1cb2359fd1e
tranhuyducseven <tranhuyducseven@gmail.com>
2021-05-18 16:38:27 +0700

    Completely fixed

--- On class/hash/1.cpp
+++ On class/hash/1.cpp
@@ -1,16 +1,60 @@
-  void insert(int key)
+#include <iostream>
+using namespace std;
+#include <list>
+class ChainingHash
+{
+private:
+    int (*hash)(int);
+    list<int> *data;
+    int size;
+
+public:
+    ChainingHash()
+    {
+        throw "Cannot create a hash with no hash function!";
+    }
+
+    ChainingHash(int size, int (*hash)(int))
+    {
+        this->hash = hash;
+        this->size = size;
+        this->data = new list<int>[size];
+    }
+    int count()
+    {
+        int total = 0;
+        for (int i = 0; i < this->size; i++)
+        {
+            total += this->data[i].size();
+        }
+        return total;
+    }
+
+    void dump()
+    {
+        for (int i = 0; i < this->size; i++)
+        {
+            cout << "Slot " << i << ": " << this->data[i].size() << " element(s)" << endl;
+        }
+    }
+    void insert(int key)
     {
         int index = this->hash(key);
-        this->data[index].push_back(key);
+        this->data[index].push_front(key);
     }
 
     void remove(int key)
     {
         int index = this->hash(key);
-        if (this->search(key))
-            this->data[index].remove(key);
-        else
-            cout << "Not found!";
+        for (list<int>::iterator it = this->data[index].begin(); it != this->data[index].end(); ++it)
+        {
+            if (*it == key)
+            {
+                this->data[index].erase(it);
+                return;
+            }
+        }
+        throw "Not found!";
     }
 
     bool search(int key)
@@ -23,3 +67,37 @@
         }
         return false;
     }
+};
+
+int hashWith11(int key)
+{
+    return key % 11;
+}
+int hashWith15(int key)
+{
+    return key % 15;
+}
+int hashWith17(int key)
+{
+    return key % 17;
+}
+int main()
+{
+
+    ChainingHash *hashTable = new ChainingHash(11, hashWith11);
+    int data[] = {28, 81, 55, 92, 33, 29, 18, 23, 40, 99, 79, 74, 49, 64, 17};
+    int size = sizeof(data) / sizeof(int);
+    for (int i = 0; i < size; i++)
+    {
+        hashTable->insert(data[i]);
+    }
+
+    int indices[] = {1, 5, 7};
+    int indicesSize = sizeof(indices) / sizeof(int);
+    for (int i = 0; i < indicesSize; i++)
+    {
+        hashTable->remove(data[indices[i]]);
+    }
+
+    hashTable->dump();
+}
--- On class/hash/2.cpp
+++ On class/hash/2.cpp
@@ -1,34 +1,122 @@
- int insert(int key)
+#include <iostream>
+using namespace std;
+enum STATUS_TYPE
+{
+    NIL,
+    NON_EMPTY,
+    DELETED
+};
+class OpenHash
+{
+private:
+    int (*hp)(int, int);
+    STATUS_TYPE *status;
+    int *data;
+    int size;
+
+public:
+    OpenHash()
     {
-        int index = 0;
+        throw "Cannot create a hash with no hash function!";
+    }
+
+    OpenHash(int size, int (*hp)(int, int))
+    {
+        this->hp = hp;
+        this->size = size;
+        this->data = new int[size];
+        this->status = new STATUS_TYPE[size];
         for (int i = 0; i < size; i++)
         {
-            index = this->hp(key, i);
-            if (index < this->size && status[index] == NIL)
-            {
-                this->data[index] = key;
-                this->status[index] = NON_EMPTY;
-                break;
-            }
-            else if (index >= this->size)
-                throw "Overflow!";
+            this->status[i] = NIL;
         }
-        return index;
+    }
+
+    int insert(int key)
+    {
+        //18/5/2021 12:08
+        int j = 0;
+        int i = 0;
+        while (i < this->size)
+        {
+            j = this->hp(key, i);
+            if (j < size && (this->status[j] == NIL || this->status[j] == DELETED))
+            {
+                this->data[j] = key;
+                this->status[j] = NON_EMPTY;
+                return j;
+            }
+            else
+                i += 1;
+        }
+        throw "Not Found!";
+        return -1;
     }
 
     void remove(int key)
     {
         int k = this->search(key);
-        this->data[k] = 0;
-        this->status[k] = DELETED;
+        if (k == -1)
+            throw "Not Found!";
+        else
+            this->status[k] = DELETED;
     }
 
     int search(int key)
     {
-        for (int i = 0; i < this->size; i++)
+        int j = 0;
+        int i = 0;
+        do
         {
-            if (this->data[i] == key)
-                return i;
-        }
+            j = this->hp(key, i);
+            if (this->data[j] == key && this->status[j] == NON_EMPTY)
+                return j;
+            i += 1;
+        } while (this->status[j] != NIL && i < this->size);
         return -1;
     }
+
+    void dump()
+    {
+        for (int i = 0; i < this->size; i++)
+        {
+            if (this->status[i] != NON_EMPTY)
+                cout << "Slot " << i << ": Empty" << endl;
+            else
+                cout << "Slot " << i << ": " << this->data[i] << endl;
+        }
+    }
+};
+
+int linearProbing(int key, int i)
+{
+    return ((key % 11) + i) % 11;
+}
+
+int quadraticProbing(int key, int i)
+{
+    return ((key % 11) + i + 3 * i * i) % 11;
+}
+
+int doubleHashing(int key, int i)
+{
+    int h1 = key % 11;
+    int h2 = 1 + (key % 10);
+    return (h1 + i * h2) % 11;
+}
+
+int main()
+{
+
+    OpenHash *hashTable = new OpenHash(11, doubleHashing);
+    int data[] = {10, 22, 31, 4, 15, 28, 17, 88, 59};
+    int size = sizeof(data) / sizeof(int);
+
+    for (int i = 0; i < size; i++)
+    {
+        hashTable->insert(data[i]);
+    }
+
+    hashTable->remove(22);
+    hashTable->dump();
+}
